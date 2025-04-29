e2e6e9ea18b9a9c69ee8ad0f951a5785cbb1fde0
Oleksandr Smirnov <olexsmir@gmail.com>
2025-04-29 23:53:54 +0300

    implement notes adding

--- anki/anki.go
+++ anki/anki.go
@@ -40,6 +40,7 @@ type (
 		DeckName  string            `json:"deckName"`
 		ModelName string            `json:"modelName"`
 		Fields    map[string]string `json:"fields"`
+		Tags      []string          `json:"tags"`
 	}
 
 	internalNote struct {
@@ -47,11 +48,16 @@ type (
 	}
 )
 
-func (a *AnkiClient) AddNote(deckName, modelName string, fields map[string]string) (NoteID, error) {
+func (a *AnkiClient) AddNote(
+	deckName, modelName string,
+	fields map[string]string,
+	tags []string,
+) (NoteID, error) {
 	res, err := request[NoteID]("addNote", internalNote{Note{
 		DeckName:  deckName,
 		ModelName: modelName,
 		Fields:    fields,
+		Tags:      tags,
 	}})
 	if err != nil {
 		return 0, err
--- main.go
+++ main.go
@@ -1,55 +1,72 @@
 package main
 
 import (
+	"fmt"
 	"log/slog"
+	"os"
 
 	"github.com/olexsmir/anpi/anki"
+	"github.com/olexsmir/anpi/parser"
 )
 
+const importFile = "import.yml"
+
 func main() {
+	if err := run(); err != nil {
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		os.Exit(1)
+	}
+}
+
+func run() error {
 	anki := anki.NewAnkiClient()
 
-	nid, err := anki.AddNote("testing", "Basic", map[string]string{
-		"Front": "the thing",
-		"Back":  "asdfasfasdf",
-	})
-	slog.Info("note added", "nid", nid, "err", err)
+	f, err := os.ReadFile(importFile)
+	if err != nil {
+		return err
+	}
 
-	// decks, err := anki.GetDeckNames()
-	// slog.Info("test deck names", "fields", decks, "err", err)
-	//
-	// fields, err := anki.GetModelFieldNames("Basic")
-	// slog.Info("test existent type", "fields", fields, "err", err)
-	//
-	// fieldsErr, err := anki.GetModelFieldNames("chicked jokey")
-	// slog.Info("test non-existent type", "fields", fieldsErr, "err", err)
-	//
-	// f, _ := os.ReadFile("test.yml")
-	// data, err := parser.Parse(f)
-	// slog.Info("parsing data", "err", err)
-	//
-	// lookup := "front"
-	// ankiField := data[1].FieldLookUp(lookup)
-	// slog.Info("looking up anki note field", "field", ankiField, "lookup", lookup)
+	data, err := parser.Parse(f)
+	if err != nil {
+		return err
+	}
 
-	// for _, deck := range data {
-	// 	fmt.Printf("\nDeck: %s\n", deck.Deck)
-	// 	fmt.Printf("Type: %s\n", deck.Type)
-	// 	fmt.Printf("Global Tags: %v\n", deck.Tags)
-	// 	fmt.Println("Fields mapping:")
-	// 	for internalName, anki := range deck.Fields {
-	// 		fmt.Printf("  %s -> %s\n", internalName, anki)
-	// 	}
-	//
-	// 	fmt.Println("Notes:")
-	// 	for _, note := range deck.Notes {
-	// 		fmt.Println("  Note fields:")
-	// 		for internalName, value := range note.Fields {
-	// 			fmt.Printf("    %s: %s\n", internalName, value)
-	// 		}
-	// 		if len(note.Tags) > 0 {
-	// 			fmt.Printf("    Local Tags: %v\n", note.Tags)
-	// 		}
-	// 	}
-	// }
+	slog.Info("data", "data", data)
+
+	for _, deck := range data {
+		for _, note := range deck.Notes {
+			fields := make(map[string]string)
+			for k, v := range note.Fields {
+				slog.Info("chicken jokey", "k", k, "v", v, "yep", deck.FieldLookUp(k))
+				fields[deck.FieldLookUp(k)] = v
+			}
+
+			slog.Info("got fields", "fields", fields)
+
+			tags := mergeTags(deck.Tags, note.Tags)
+			if _, err := anki.AddNote(deck.Deck, deck.Type, fields, tags); err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
+}
+
+func mergeTags(global, local []string) []string {
+	unique := make(map[string]struct{})
+
+	for _, tag := range global {
+		unique[tag] = struct{}{}
+	}
+	for _, tag := range local {
+		unique[tag] = struct{}{}
+	}
+
+	result := make([]string, 0, len(unique))
+	for tag := range unique {
+		result = append(result, tag)
+	}
+
+	return result
 }
